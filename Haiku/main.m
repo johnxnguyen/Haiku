084f10c5794d38b9a1a20b638f75137881ea6199
John Nguyen <johnxuannguyen@gmail.com>
2014-12-05 21:28:39 +0100

    Initial Commit

---
+++ Haiku/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Haiku
+//
+//  Created by John Nguyen on 5/12/2014.
+//  Copyright (c) 2014 John Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
